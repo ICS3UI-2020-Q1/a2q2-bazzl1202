c5a0900419f191c89051217a431353e810bbe2ed
bazzl1202 <bazzl1202@wrdsb.ca>
2020-09-15 16:19:21 +0000

    Now tells user what number is biggest.

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * tells the user which number is bigger
+ * @author Liana Bazzarella
  */
 public class Main {
 
@@ -9,7 +10,29 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // Create a scanner for user input
+    Scanner input = new Scanner(System.in);
+
+    //ask the user to enter the first integer
+    System.out.println("Please enter an integer:");
+
+    //declare and initialize variable to store integer one
+    int integerOne = input.nextInt();
+
+    //ask the user to enter the second integer
+    System.out.println("Please enter another integer:");
+
+    //declare and initialize variable to store integer two
+    int integerTwo = input.nextInt();
     
+    // tells the user what number is the biggest
+    if( integerOne > integerTwo ){
+      System.out.println("The biggest number is " + integerOne + ".");
+    } else if( integerOne < integerTwo ){
+      System.out.println("The biggest number is " + integerTwo + ".");
+    } else {
+      // tells the user there is no biggest number
+      System.out.println("There is no biggest number.");
+    }
   }
 }
